29fa1b13a3b04db0de422a9901144464fc763467
Daniel <daniel.fnz@hotmail.com>
2016-03-26 15:20:28 -0300

    Revert "Correção e comentarios da tradução para linguagem FORTRAN."
    This reverts commit 830d4c1a261ec12718e8557209d77f9bc6b6e348.

--- src/tradutorlinguagens/LinguagemFortran.java
+++ src/tradutorlinguagens/LinguagemFortran.java
@@ -14,15 +14,14 @@ import tradutorlinguagens.Observer.Observador;
  * @author TheOwnzs
  */
 public class LinguagemFortran implements Observador {
-    Main main;
     private String Linguagem;
     private List<String> lista;
 
     public LinguagemFortran() {
     }
 
-    public LinguagemFortran(Main main) {
-        this.main = main;
+    public LinguagemFortran(String Linguagem) {
+        this.Linguagem = Linguagem;
     }
 
     public String getLinguagem() {
@@ -33,58 +32,12 @@ public class LinguagemFortran implements Observador {
         this.Linguagem = Linguagem;
     }    
 
-   @Override
+    @Override
     public void atualizar(String Ling) {
-        
-       //Expressao regular, Se tiver " " 
-       if(Ling.matches("\"\\w+\"")){  
-           //Insere no JTexArea
-           setLinguagem("Print *, "+Ling+"\n");   
-      } 
-       
-       //Expressao regular para validar atribuicao inteiro x = 5
-       if(Ling.matches("\\w( )[=]( )\\d+|\\w()[=]()\\d+")){
-          //Formula a variavel em fortran;
-          String Inteiros = "INTEGER :: "+Ling.substring(0, Ling.lastIndexOf("="));   
-          //Insere a variavel formulada no JTexArea (Main)
-          main.inserirVariavel(Inteiros); 
-          //Atribui valor a variavel
-          setLinguagem(Ling+"\n");
-       }    
-       //Expressão regular para validar atribuicao float, usando virgula.
-       if(Ling.matches("\\w( )[=]( )\\d+,\\d+|\\w()[=]()\\d+,\\d+")){
-           //Formula a variavel em fortran;
-           String Floats = "REAL :: "+Ling.substring(0, Ling.lastIndexOf("=")); 
-           //Insere a variavel formulada no JTexArea (Main)
-           main.inserirVariavel(Floats); 
-            //Atribui valor a variavel
-           setLinguagem(Ling+"\n");
-       }
-     
-       //Expressão regular para validar atribuicao String.
-       if(Ling.matches("\\w( )[=]( )\"\\D+\"|\\w()[=]()\"\\D+\"")){
-          //Formula a variavel em fortran;
-          String Caracteres = "CHARACTER :: "+Ling.substring(0, Ling.lastIndexOf("="));   
-          //Insere a variavel formulada no JTexArea (Main)
-          main.inserirVariavel(Caracteres); 
-          //Atribui valor a variavel
-          setLinguagem(Ling+"\n");
-       }
-        //Expressao regular para validar o Laco de repetição, aceitando 4 espaços entre palavras     
-        if(Ling.matches("Repita \\d+x: \"\\w+\"|Repita \\d+x: \"\\w+\\s\\w+\"|Repita \\d+x: \"\\w+\\s\\w+\\s\\w+\"|Repita \\d+x: \"\\w+\\s\\w+\\s\\w+\\s\\w+\"")){
-            //Formula a variavel em fortran;
-            String Inteiros = "INTEGER :: i"; 
-            //Insere a variavel formulada no JTexArea (Main)
-            main.inserirVariavel(Inteiros); 
-            //Pega o valor do x vezes            
-            String i = Ling.substring(Ling.indexOf("a ")+2, Ling.indexOf("x:"));
-             //Pega o texto
-            String texto = Ling.substring(Ling.indexOf("\""), Ling.lastIndexOf("\"")+1);
-            //Monta o for com as variaveis            
-            String Montar = "do i = 1, "+i+"\n Print *, "+texto+"\nend do\n"; 
-            //Seta a linguagem
-            setLinguagem(Montar);            
-        }
+       if(Ling.contains("\"")){   
+          
+           setLinguagem("Print *, "+Ling+"\n");     
+             }   
     }
 }
 
